b004c14201c739239c3612d6b50ca5dd01802205
Afrozalam123 <alamafroz70400@gmail.com>
2025-08-23 23:11:00 +0530

    updated new code and code existing

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div></div>
+    <div>welocme my home page</div>
   )
 }
 
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -3,26 +3,36 @@ import  "../pages/Navbar.css";
 export default function Navbar() {
   return (
      <div>
-     
-
- <nav className="  sm:bg-amber-600   lg:bg-gray-400 ring-4">
-     <ul className=" sm:text-red-950 text-xs ml-44 p-8   flex relative  w-20 md:text-blue-900  outline-slate-600 md:text-xl    lg:text-2xl mix-blend-overlay">
-       <NavLink to="/" end>Home</NavLink> 
-   <NavLink to="/aboutus">Aboutus</NavLink> 
-        <NavLink to="/contact">Contact</NavLink> 
-         <NavLink to="/menu">Menu</NavLink> 
-            <NavLink to="/login">Login</NavLink>  
+ <nav className="sm:bg-amber-600   lg:bg-gray-400 ring-4">
+     <ul className=" sm:text-red-950  ml-4 text-xl  p-4 flex  w-20 md:text-blue-900  outline-slate-600 md:text-xl lg:text-2xl m-40  cursor-pointer">
+      <li> <NavLink to="/" end>Home</NavLink> </li>
+     <li> <NavLink to="/aboutus">Aboutus</NavLink> </li>
+      <li>  <NavLink to="/contact">Contact</NavLink> </li>
+       <li>   <NavLink to="/menu">Menu</NavLink> </li>
+        <li>    <NavLink to="/login">Login</NavLink>  </li>
      </ul>
       
     </nav>
-
-
-
    <section className="logo">
      <h1>Foodie</h1>
    </section>
 
+    <section className="icon">
+    <i class="fa-solid fa-magnifying-glass"></i>
+   </section>
+   <div className="cart-container">
+    <i class="fa-solid fa-cart-shopping "></i>
+    <div className="cart-dropdown">
+     <h1>Cart Empty</h1>
+     <p>Good food is always cooking! Go ahead, order some yummy items from the menu</p>
     </div>
+   </div>
+    
+
+
+    
+</div>
+    
      
      
 
