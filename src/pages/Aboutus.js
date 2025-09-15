b4b3af97c6da2d411f25b15e97e7205ddc530e87
Afrozalam123 <alamafroz70400@gmail.com>
2025-09-15 16:16:44 +0530

    please update my new code and old code

--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import "../Global.css/About.css"
+import Banner1 from "../Afroz/Banner1.jpg"
 const Aboutus = () => {
   return (
-    <div>Aboutus page welcome</div>
+    <div>
+     <img src={Banner1} alt='Banner1' className='Banner'/>
+     <div className="banr-cont">
+       <h1>Welcome to About us</h1>
+     <p>Welcome to Foodie<br/> where every meal is made with love and freshness.</p>
+     <button className="btn-explore">Explore More</button>
+     </div>
+    </div>
   )
 }
 
